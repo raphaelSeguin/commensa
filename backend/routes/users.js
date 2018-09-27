c485b56231530011210827839b89f33e44cc3563
raphaelSeguin <raphaelseguin@gmail.com>
2018-09-27 18:57:14 +0200

    huuu

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,4 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  req.send('i am a user, duh!');
-});
-
 module.exports = router;
